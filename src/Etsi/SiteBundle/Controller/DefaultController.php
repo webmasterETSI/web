67fc0a04757387ab7e5bcc0f46bbb317923fdd41
NaxYo <z-i_r_o@hotmail.com>
2012-12-18 12:07:09 +0100

    Añadido filtro assetic de ficheros .less

--- src/Etsi/SiteBundle/Controller/DefaultController.php
+++ src/Etsi/SiteBundle/Controller/DefaultController.php
@@ -7,29 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller,
 
 use FOS\RestBundle\View\View;
 
-use Etsi\SiteBundle\Form\UsuarioType,
-    Etsi\SiteBundle\Form\SeccionType,
-    Etsi\SiteBundle\Form\ContenidoType;
-
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        /*
-        $data = $this->createForm(new UsuarioType());
-        $view = new View($data);
-        $view->setTemplate('EtsiSiteBundle:Form:newUsuario.html.twig');
-        */
-        /*
-        $data = $this->createForm(new ContenidoType());
-        $view = new View($data);
-        $view->setTemplate('EtsiSiteBundle:Form:newContenido.html.twig');
-        return $this->get('fos_rest.view_handler')->handle($view);
-        */
-        $data = $this->createForm(new SeccionType());
-        $view = new View($data);
-        $view->setTemplate('EtsiSiteBundle:Form:newSeccion.html.twig');
-        return $this->get('fos_rest.view_handler')->handle($view);
+        return $this->render('EtsiSiteBundle:Default:index.html.twig');
     }
 }

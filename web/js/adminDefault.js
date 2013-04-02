9549009c90118246fd934331a78d9d6e85d9e37f
NaxYo <ziroargentino@gmail.com>
2013-04-02 14:34:16 +0200

    Añadido template libre básico

---
+++ web/js/adminDefault.js
@@ -0,0 +1,19 @@
+$(function(){
+	$(".tablesorter").tablesorter(); 
+
+	$(".tab_content").hide();
+	$("ul.tabs li:first").addClass("active").show();
+	$(".tab_content:first").show();
+
+	$("ul.tabs li").click(function() {
+		$("ul.tabs li").removeClass("active");
+		$(this).addClass("active");
+		$(".tab_content").hide();
+
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
+
+	$('.column').equalHeight();
+});

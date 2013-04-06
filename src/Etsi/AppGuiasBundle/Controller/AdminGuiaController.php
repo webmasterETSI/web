adeabc5b6940ed65751dd3e32633f5bf39966b5e
NaxYo <z_i_r_o@hotmail.com>
2013-04-06 12:37:40 +0200

    Reubicados ficheros .js y .css en el directorio apropiado

--- src/Etsi/AppGuiasBundle/Controller/AdminGuiaController.php
+++ src/Etsi/AppGuiasBundle/Controller/AdminGuiaController.php
@@ -12,15 +12,9 @@ use Etsi\AppGuiasBundle\Entity\Guia;
 
 class AdminGuiaController extends Controller
 {
-    private function renderReturn(
-        $action,
-        $id = null,
-        $messages = array('success' => array(), 'warning' => array(), 'error' => array())
-    ) {
+    private function renderReturn( $messages = array('success' => array(), 'warning' => array(), 'error' => array()) ) {
         $em = $this->getDoctrine()->getManager();
-
         $entities = $em->getRepository('EtsiAppGuiasBundle:Guia')->findAll();
-        $entity = $id?$em->getRepository('EtsiAppGuiasBundle:Guia')->find($id):null;
 
         if(!$messages) {
             $messages = array(
@@ -32,104 +26,20 @@ class AdminGuiaController extends Controller
         return $this->render(
             'EtsiAppGuiasBundle:Admin:guia.html.twig',
             array(
-                'action' => $action,
                 'messages' => $messages,
                 'entities' => $entities,
-                'entity' => $entity,
             )
         );
     }
 
-    private function fillEntity($entity, $data) {
-        $camposObligatorios = array(
-            'nombre',
-            'nombreI',
-            'codigo',
-            'caracter',
-            'creditos',
-            'departamento',
-            'area',
-            'curso',
-            'cuatrimestre'
-        );
-
-        if(Herramientas::allFields($camposObligatorios, $data)) {
-            $entity->setNombre($data['nombre']);
-            $entity->setNombreI($data['nombreI']);
-            $entity->setCodigo($data['codigo']);
-            $entity->setCaracter($data['caracter']);
-            $entity->setCreditos($data['creditos']);
-            $entity->setDepartamento($data['departamento']);
-            $entity->setArea($data['area']);
-            $entity->setCurso($data['curso']);
-            $entity->setCuatrimestre($data['cuatrimestre']);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return true;
-        }
-        return false;
-    }
-
     public function indexAction()
     {
-        return $this->renderReturn($this->generateUrl('eaga_guia_new'));
+        return $this->renderReturn();
     }
 
-    public function newAction(Request $request)
+    public function editAction($id, $paso, Request $request)
     {
-        $messages = array(
-            'success' => array(),
-            'warning' => array(),
-            'error' => array(),
-        );
-
-        $entity  = new Guia();
-        $data = $request->request->all();
-
-        if($this->fillEntity($entity, $data)) {
-            $messages['success'][] = 'Guia guardada correctamente';
-        } else {
-            $messages['error'][] = 'Faltan campos obligatorios, guia no guardada';
-        }
-
-        return $this->renderReturn(
-            $this->generateUrl('eaga_guia_edit', array('id' => $entity->getId())),
-            $entity->getId(),
-            $messages
-        );
-    }
-
-    public function editAction($id, Request $request)
-    {
-        $messages = array(
-            'success' => array(),
-            'warning' => array(),
-            'error' => array(),
-        );
-
-        $data = $request->request->all();
-        $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository('EtsiAppGuiasBundle:Guia')->find($id);
-
-        if(!$entity) {
-            $messages['error'][] = 'Entidad no encontrada';
-        } else {
-            if($this->fillEntity($entity, $data)) {
-                $messages['success'][] = 'Cambios guardada correctamente';
-            } else {
-                if(isset($data['nombre']))
-                    $messages['error'][] = 'Faltan campos obligatorios, cambios no guardada';
-            }
-        }
-        
-        return $this->renderReturn(
-            $this->generateUrl('eaga_guia_edit', array('id' => $id)),
-            $id,
-            $messages
-        );
+        return new Response("Edit ".$id.", paso: ".$paso);
     }
 
     public function deleteAction($id)
@@ -152,10 +62,6 @@ class AdminGuiaController extends Controller
             $messages['error'][] = 'Entidad no encontrada';
         }
 
-        return $this->renderReturn(
-            $this->generateUrl('eaga_guia_new'),
-            null,
-            $messages
-        );
+        return $this->renderReturn($messages);
     }
 }

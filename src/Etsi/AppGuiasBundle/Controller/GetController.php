db98928b382e84acf6b548cb140ba0e2552a09a0
NaxYo <z_i_r_o@hotmail.com>
2013-04-07 15:09:55 +0200

    Añadidos métodos de busqueda instantanea con json response. Cambiado el método get de semanas a una forma más 'semanticamente correcta'

--- src/Etsi/AppGuiasBundle/Controller/GetController.php
+++ src/Etsi/AppGuiasBundle/Controller/GetController.php
@@ -8,25 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller,
 
 use Etsi\AppGuiasBundle\Entity\Asignatura,
     Etsi\AppGuiasBundle\Entity\Competencia,
-    Etsi\AppGuiasBundle\Entity\Grado,
     Etsi\AppGuiasBundle\Entity\Profesor,
     Etsi\AppGuiasBundle\Entity\Semana;
 
 class GetController extends Controller
 {
-    public function jsonResponse($entidad, $id)
+    public function jsonResponse($data)
     {
         $response = new Response();
 
-        $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository($entidad)->find($id);
-
-        if ($entity) {
+        if ($data) {
             $serializer = $this->get('jms_serializer');
 
             $response->setStatusCode('200');
             $response->headers->set('Content-Type', 'application/json');
-            $response->setContent($serializer->serialize($entity, 'json'));
+            $response->setContent($serializer->serialize($data, 'json'));
         } else {
             $response->setStatusCode('400');
         }
@@ -36,21 +32,112 @@ class GetController extends Controller
 
     public function getAsignaturaAction($id)
     {
-        return $this->jsonResponse('EtsiAppGuiasBundle:Asignatura', $id);
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('EtsiAppGuiasBundle:Asignatura')->find($id);
+        return $this->jsonResponse($entity);
     }
 
     public function getProfesorAction($id)
     {
-        return $this->jsonResponse('EtsiAppGuiasBundle:Profesor', $id);
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('EtsiAppGuiasBundle:Profesor')->find($id);
+        return $this->jsonResponse($entity);
     }
 
     public function getCompetenciaAction($id)
     {
-        return $this->jsonResponse('EtsiAppGuiasBundle:Competencia', $id);
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('EtsiAppGuiasBundle:Competencia')->find($id);
+        return $this->jsonResponse($entity);
     }
 
-    public function getSemanaAction($id)
+    public function getSemanasAction($id)
     {
-        return $this->jsonResponse('EtsiAppGuiasBundle:Semana', $id);
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository('EtsiAppGuiasBundle:Semana')->findByGuia($id);
+        return $this->jsonResponse($entities);
+    }
+
+    public function searchAsignaturaAction(Request $request)
+    {
+        $datos = json_decode($request->getContent());
+        $repositorio = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('EtsiAppGuiasBundle:Asignatura');
+
+        switch($datos->key) {
+            case 'nombre':
+                $query = $repositorio->createQueryBuilder('a')
+                    ->where('a.nombre LIKE :nombre')
+                    ->setParameter('nombre', '%'.$datos->value.'%')
+                    ->getQuery();
+
+                $entities = $query->getResult();
+                break;
+
+            case 'codigo':
+                $entities = $repositorio->findByCodigo($datos->value);
+                break;
+        }
+
+        return $this->jsonResponse($entities);
+    }
+
+    public function searchProfesorAction(Request $request)
+    {
+        $datos = json_decode($request->getContent());
+        $repositorio = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('EtsiAppGuiasBundle:Profesor');
+
+        switch($datos->key) {
+            case 'nombre':
+                $query = $repositorio->createQueryBuilder('p')
+                    ->where('p.nombre LIKE :nombre')
+                    ->setParameter('nombre', '%'.$datos->value.'%')
+                    ->getQuery();
+
+                $entities = $query->getResult();
+                break;
+
+            case 'email':
+                $query = $repositorio->createQueryBuilder('p')
+                    ->where('p.email LIKE :email')
+                    ->setParameter('email', $datos->value.'%')
+                    ->getQuery();
+
+                $entities = $query->getResult();
+                break;
+        }
+
+        return $this->jsonResponse($entities);
+    }
+
+    public function searchCompetenciaAction(Request $request)
+    {
+        $datos = json_decode($request->getContent());
+        $repositorio = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('EtsiAppGuiasBundle:Competencia');
+
+        switch($datos->key) {
+            case 'nombre':
+                $query = $repositorio->createQueryBuilder('c')
+                    ->where('c.nombre LIKE :nombre')
+                    ->setParameter('nombre', '%'.$datos->value.'%')
+                    ->getQuery();
+
+                $entities = $query->getResult();
+                break;
+
+            case 'codigo':
+                $entities = $repositorio->findByCodigo($datos->value);
+                break;
+        }
+
+        return $this->jsonResponse($entities);
     }
 }

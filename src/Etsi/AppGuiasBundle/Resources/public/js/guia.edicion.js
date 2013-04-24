208a705e8a0baa512cbd2e0d3713db50116b5d63
NaxYo <z_i_r_o@hotmail.com>
2013-04-24 11:45:05 +0200

    Añadidas comprobaciones dinámicas (js, client-side) de datos de guía, con refresco de miñaturas

--- src/Etsi/AppGuiasBundle/Resources/public/js/guia.edicion.js
+++ src/Etsi/AppGuiasBundle/Resources/public/js/guia.edicion.js
@@ -6,13 +6,7 @@ GUIA.cambios = function(elemento) {
 	if(GUIA.saveTimeout)
 		window.clearTimeout(GUIA.saveTimeout);
 	GUIA.saveTimeout = window.setTimeout(GUIA.saveCambios, 5000);
-
-	var contenedor = elemento.parents('div.contenedor').first();
-	if(contenedor.hasClass('empty')) {
-		contenedor.removeClass('empty');
-		$('.navigation > ul').miniaturiza('refresh');
-	}
-}
+};
 
 
 GUIA.saveCambios = function() {
@@ -67,7 +61,7 @@ GUIA.saveCambios = function() {
 
 	if(GUIA.saveTimeout)
 		window.clearTimeout(GUIA.saveTimeout);
-}
+};
 
 GUIA.updateData = function(data) {
 	$.ajax({
@@ -77,22 +71,100 @@ GUIA.updateData = function(data) {
 		contentType : 'application/json',
 		cache: false
 	});
-}
+};
 
+GUIA.testSemanas = function() {
+	var totales = {
+		t:  parseFloat($('#creditosTeoricos').val().replace(',', '.'))*10,
+		pa: parseFloat($('#creditosPracticosAula').val().replace(',', '.'))*10,
+		pc: parseFloat($('#creditosPracticosCampo').val().replace(',', '.'))*10,
+		pi: parseFloat($('#creditosPracticosInformatica').val().replace(',', '.'))*10,
+		pl: parseFloat($('#creditosPracticosLaboratorio').val().replace(',', '.'))*10
+	};
+
+	$('#total-aula').text(totales.pa);
+	$('#total-campo').text(totales.pc);
+	$('#total-clases').text(totales.t);
+	$('#total-informatica').text(totales.pi);
+	$('#total-laboratorio').text(totales.pl);
+
+	var registrados = [0, 0, 0, 0, 0];
+	$('#tabla-semanas tbody tr').each(function() {
+		$(this).children('td').children('.mini').each(function(index) {
+			registrados[index] += parseFloat($(this).val().replace(',', '.'));
+		});
+	});
+
+	var errores = false;
+	function setError(index, er) {
+		if(er) {
+			errores = true;
+			$('#tabla-semanas tfoot tr td').eq(index).addClass('error');
+		} else 
+			$('#tabla-semanas tfoot tr td').eq(index).removeClass('error');
+	}
+
+	setError(1, registrados[0]!=totales.t);
+	setError(2, registrados[1]!=totales.pa);
+	setError(3, registrados[2]!=totales.pi);
+	setError(4, registrados[3]!=totales.pl);
+	setError(5, registrados[4]!=totales.pc);
+
+	var contenedor = $('#tabla-semanas').closest('.contenedor');
+	if(errores) contenedor.addClass('empty');
+	else contenedor.removeClass('empty');
+	
+	$('.navigation > ul').miniaturiza('refresh');
+};
+
+GUIA.testText = function(elemento) {
+	var contenedor = $(elemento).closest('.contenedor');
+	if(elemento.text() == "") contenedor.addClass('empty');
+	else contenedor.removeClass('empty');
+	
+	$('.navigation > ul').miniaturiza('refresh')
+};
+
+GUIA.testInput = function(elemento) {
+	var contenedor = $(elemento).closest('.contenedor');
+	if(elemento.val() == "") contenedor.addClass('empty');
+	else contenedor.removeClass('empty');
+	
+	$('.navigation > ul').miniaturiza('refresh')
+};
+
+GUIA.testSelect = function(elemento) {
+	var contenedor = $(elemento).closest('.contenedor');
+	if(elemento.children(':selected').length == 0) contenedor.addClass('empty');
+	else contenedor.removeClass('empty');
+
+	$('.navigation > ul').miniaturiza('refresh')
+};
+
+GUIA.testCreditos = function(elemento) {
+	$(elemento).val($(elemento).val().replace(',', '.'));
+	var total = parseFloat($(elemento).val());
+	$(elemento).siblings('input').each(function() {
+		total += parseFloat($(this).val());
+	});
+
+	var contenedor = $(elemento).closest('.contenedor');
+	if( total != parseFloat($('#creditos-totales').text()) ) {
+		contenedor.addClass('empty');
+		$('#creditos-totales').addClass('error');
+	} else {
+		contenedor.removeClass('empty');
+		$('#creditos-totales').removeClass('error');
+	}
+	
+	$('.navigation > ul').miniaturiza('refresh')
+};
 
 $(function(){
 	// Calculo de créditos correctos
 	$('#contenedor-creditos > input').bind('keyup', function() {
-		$(this).val($(this).val().replace(',', '.'));
-		var total = parseFloat($(this).val());
-		$(this).siblings('input').each(function() {
-			total += parseFloat($(this).val());
-		});
-
-		if( total != parseFloat($('#creditos-totales').text()) )
-			$('#creditos-totales').addClass('error');
-		else
-			$('#creditos-totales').removeClass('error');
+		GUIA.testCreditos($(this));
+		GUIA.testSemanas();
 	}).
 	first().trigger('keyup');
 
@@ -102,6 +174,7 @@ $(function(){
 		.change(function() {
 			$(this).addClass('cambios-no-guardados').removeClass('cambios-guardados');
 			GUIA.cambios($(this));
+			GUIA.testSelect($(this));
 		});
 
 	$('.editor-minimo').bind('keyup', function() {
@@ -109,10 +182,15 @@ $(function(){
 		GUIA.cambios($(this));
 	});
 
+	$('#nombreI').bind('keyup', function() {
+		GUIA.testInput($(this));
+	});
+
 	$('.editor-semana').bind('keyup', function() {
 		var fila = $(this).closest('tr'); 
 		fila.addClass('cambios-no-guardados').removeClass('cambios-guardados');
 		GUIA.cambios(fila);
+		GUIA.testSemanas();
 	});
 
 	var data = $('#datosEspecificos_6_1_1 > .hidden').text();
@@ -160,5 +238,6 @@ CKEDITOR.on( 'instanceCreated', function( e ) {
 
 	jElement.bind('keyup', function(){
 		$(this).addClass('cambios-no-guardados').removeClass('cambios-guardados');
+		GUIA.testText($(this));
 	});
 });

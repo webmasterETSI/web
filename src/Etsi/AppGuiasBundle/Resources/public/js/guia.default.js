2bdc0d7500afd0a648708ef688572ae35d9b2dce
NaxYo <z_i_r_o@hotmail.com>
2013-05-03 12:36:09 +0200

    Añadido formulario de beta feedback

--- src/Etsi/AppGuiasBundle/Resources/public/js/guia.default.js
+++ src/Etsi/AppGuiasBundle/Resources/public/js/guia.default.js
@@ -1,3 +1,54 @@
 $(function(){
 	$('.temporal').delay(5000).hide('slow');
+
+
+	$('#feedback').click(function() {
+		$('#opinion').toggle();
+	});
+
+	$('#opinion').on('mouseleave', function() {
+		$(this).hide();
+	});
+
+	$('#opinion').on('submit', function() {
+		var form = $(this);
+		var action = form.attr('action');
+		var method = form.attr('method');
+
+		var data = {};
+		data.tipo = form.children('input[type="radio"]:checked').val();
+		data.texto = form.children('textarea').val();
+
+		$(this).children('img').show();
+		$.ajax({
+			type: method,
+			url: action,
+			data: JSON.stringify(data),
+			contentType : 'application/json',
+			cache: false,
+
+			error: function(data) {
+				var error = $('<h4></h4>')
+					.addClass('alert_error')
+					.text('Algo va mal... intentalo nuevamente más tarde')
+					.delay(5000).hide('slow');
+
+				$('#wrapper').prepend(error);
+			},
+			success: function(data) {
+				var info = $('<h4></h4>')
+					.addClass('alert_info')
+					.text('¡Gracias por tu opinión!')
+					.delay(5000).hide('slow');
+
+				$('#wrapper').prepend(info);
+			},
+			complete: function() {
+				form.hide();
+				form.children('img').hide();				
+			}
+		})
+
+		return false;
+	});
 });
